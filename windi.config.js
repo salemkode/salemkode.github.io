8f1fb81a66a3a6f117f9a51ba987fb4cbeede398
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2021-11-10 13:39:59 +0300

    Update windi.config.js

--- windi.config.js
+++ windi.config.js
@@ -4,8 +4,8 @@ export default {
       center: true,
       padding: {
         DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
+        sm: '1rem',
+        lg: '3rem',
         xl: '5rem',
         '2xl': '6rem',
       },
