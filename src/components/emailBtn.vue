137e9a90c51f2e90770b7c6ee00b4e4867bc5400
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2021-11-13 12:00:29 +0300

    make popup for email

--- src/components/emailBtn.vue
+++ src/components/emailBtn.vue
@@ -1,24 +1,62 @@
 <template>
-    <a class="btn-email btn" href="mailto:kodesalem@gmail.com">
-        <strong>{{ text }}</strong>
-        <img src="landing/next.svg" class="icon" />
+  <Fragment>
+    <a @click="showPopup" :class="'btn ' + _class" :href="'mailto:' + email">
+      <strong>{{ text }}</strong>
+      <img src="landing/next.svg" class="icon" />
     </a>
+    <pop-up :show="popup" @close="popup = !popup;">
+      <img src="mail.svg" class="p-3 h-1/2 m-auto" alt />
+      <h1 class="mt-3 p-3 bg-dark-200 rounded-md text-white bold" v-text="email" />
+      <button @click="copyMail()" class="btn btn-mail">Copy my mail</button>
+    </pop-up>
+    <transition name="fade">
+      <Coped v-show="alert" />
+    </transition>
+  </Fragment>
 </template>
 
 <script>
+import { Fragment } from 'vue-fragment';
+
 export default {
-    props: ["text"]
+  props: ["text", "_class"],
+  data() {
+    return {
+      email: "kodesalem@gmail.com",
+      popup: false,
+      alert: false
+    }
+  },
+  methods: {
+    showPopup() {
+      this.popup = !this.popup;
+      this.copyMail()
+    },
+    copyMail() {
+      navigator.clipboard.writeText(this.email);
+      if (!this.alert) {
+        this.alert = true;
+        setTimeout(e => this.alert = false, 500)
+      }
+    }
+  },
+  components: {
+    Fragment
+  }
 }
 </script>
 
 <style scoped>
 .btn {
-  @apply "bg-main text-white rounded-full p-3 px-5 flex items-center w-max text-sm";
+  @apply "bg-main text-white rounded-full p-3 md:px-4 px-3 flex items-center w-max text-sm";
 }
-.btn-email .icon {
-  @apply "px-3 pt-0.5 transition transition-all";
+.btn-mail {
+  @apply "px-8 mt-4 font-bold";
 }
-.btn-email:hover .icon {
-  @apply "pl-6";
+.btn .icon {
+  @apply "px-2 md:px-3 pt-0.5 transition transition-all";
+}
+.btn:hover .icon {
+  @apply "sm:pl-6";
 }
 </style>
---
+++ src/components/popUp.vue
@@ -0,0 +1,36 @@
+<template>
+    <Fragment>
+        <transition name="fade">
+            <div class="popup center-position shadow-card" v-show="show">
+                <slot />
+            </div>
+        </transition>
+        <transition name="fade">
+            <span
+                class="popup-shadow center-position"
+                v-show="show"
+                @click="$emit('close', $event.target.value)"
+            />
+        </transition>
+    </Fragment>
+</template>
+
+<script>
+import { Fragment } from 'vue-fragment';
+export default {
+    props: ["show"],
+    components: {
+        Fragment
+    }
+}
+</script>
+
+<style scoped>
+.popup {
+    @apply "fixed flex flex-col bg-white p-4 rounded-md text-black w-[320px] h-[350px] z-60";
+}
+.popup-shadow {
+    @apply "fixed bg-white bg-opacity-5 w-full h-full z-50";
+    backdrop-filter: blur(3px);
+}
+</style>
