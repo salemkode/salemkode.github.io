4336f3d2c4c9438514df80f118c238d77f2ac19a
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2021-11-13 11:59:52 +0300

    add coped components

---
+++ src/components/coped.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1 class="center-position">My mail is coped</h1>
+</template>
+
+<script>
+export default {
+    props: [
+        "show"
+    ]
+}
+</script>
+
+<style scoped>
+h1 {
+    @apply "fixed top-[90%] z-50 bg-dark-200 bg-opacity-80 rounded-full py-3 px-6 text-white";
+}
+</style>
