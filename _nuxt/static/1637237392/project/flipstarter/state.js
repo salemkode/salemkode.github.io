0c6f496edd820ff7b8b93f87f49f07c578c21d11
salemkode <salemkode@users.noreply.github.com>
2021-11-18 12:27:46 +0000

    deploy: def7b3edbdf695ee4a9c056031d331e80eb88ab1

--- _nuxt/static/1637237392/project/flipstarter/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637237392",layout:"default",error:a,serverRendered:true,routePath:"\u002Fproject\u002Fflipstarter",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3025bc7e"}}}}(null));
---
+++ _nuxt/static/1637238465/project/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637238465",layout:"default",error:a,serverRendered:true,routePath:"\u002Fproject",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c8c56fc1"}}}}(null));
