6ddb8b146ee8a70264171c3e73edda4a05ecfdaa
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2021-11-18 13:53:41 +0300

    add /project page

---
+++ src/pages/project/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <a :href="project.path" v-for="project in projects" :key="project.title" class="card">
+            <img :src="project.image" />
+            <div class="px-8 my-3">
+                <h1 class="text-2xl mb-3">{{ project.title }}</h1>
+                <p>{{ project.description }}</p>
+            </div>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    async asyncData({ $content }) {
+        const projects = await $content('project')
+            .only(["title", "description", "image", "path"])
+            .fetch()
+
+        return {
+            projects
+        }
+    },
+}
+</script>
+
+<style>
+header {
+    position: sticky !important;
+    top: 0;
+}
+</style>
+
+<style scoped>
+.card {
+    @apply "border block border-gray-400 rounded-lg overflow-hidden shadow-lg md:flex items-center mb-3";
+}
+.card img {
+    @apply "w-full md:w-1/3 h-[150px] md:h-[200px] object-cover";
+}
+</style>
