92193a6b7d5dc601aebe08641e8a8c3ff9971942
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2021-11-18 13:50:34 +0300

    make card containter component and use it in service section

---
+++ src/components/cardContainter.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="card-container">
+        <a :href="card.path" class="card" v-for="card in data" :key="card.title">
+            <img v-if="img" :src="img(card)" alt :class="size === 'large' ? 'large' : ''"/>
+            <div class="info">
+                <h3 v-text="card.title" />
+                <p v-text="card.description" />
+            </div>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted(){
+        console.log(this.size , ":" , this.size === 'large' ? 'large' : '')
+    },
+    props: [
+        "data",
+        "img",
+        "size",
+    ]
+}
+</script>
+
+<style scoped>
+.card-container {
+    @apply "md:flex relative";
+}
+.card-container::before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background: #606060;
+    opacity: 0.18;
+    filter: blur(25px);
+    transform: scaleX(1.1);
+}
+.card {
+    @apply "md:w-1/3 shadow-card rounded-lg bg-white relative overflow-hidden block";
+}
+.card img.large{
+    @apply "h-[150px] w-full object-cover";
+}
+.card img.large ~ .info{
+    @apply "pt-3";
+}
+.card .info {
+    @apply "pb-8 px-6";
+}
+.card:nth-child(2) {
+    @apply "my-6 md:mx-6 md:my-0";
+}
+.card > * {
+    @apply "text-center mx-auto";
+}
+.card h3 {
+    @apply "mb-4 text-xl";
+}
+</style>
--- src/components/section/service.vue
+++ src/components/section/service.vue
@@ -2,13 +2,7 @@
     <section class="section-service">
         <h1>I develop and maintain</h1>
         <div class="content">
-            <div class="card-container">
-                <div class="card" v-for="service in services" :key="service.title">
-                    <img :src="'service/' + service.title + '.svg'" alt />
-                    <h3 v-text="service.title" />
-                    <p v-text="service.info" />
-                </div>
-            </div>
+            <card-containter :data="services" :img="img"/>
         </div>
     </section>
 </template>
@@ -20,17 +14,22 @@ export default {
             services: [
                 {
                     title: "Websites",
-                    info: "I mainly develop full dynamic easy to use websites."
+                    description: "I mainly develop full dynamic easy to use websites."
                 },
                 {
                     title: "Blockchain",
-                    info: "Building and dealing with smart contracts is the future."
+                    description: "Building and dealing with smart contracts is the future."
                 },
                 {
                     title: "Bot",
-                    info: "I develop telegram bots to do some crazy things sometimes."
+                    description: "I develop telegram bots to do some crazy things sometimes."
                 }
             ]
+        }
+    },
+    methods : {
+        img(service){
+            return 'service/' + service.title + '.svg'
         }
     }
 }
@@ -40,41 +39,10 @@ export default {
 .section-service {
     @apply "container py-10 bg-opacity-5 overflow-hidden";
 }
-.section-service h1 {
-    @apply "text-3xl";
-}
 .section-service .content {
     @apply "md:flex py-15 relative";
 }
 .section-service .content {
     @apply "py-15";
-}
-.section-service .content > .card-container {
-    @apply "md:flex relative";
-}
-.section-service .content > .card-container::before {
-    content: "";
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    background: #606060;
-    opacity: 0.18;
-    filter: blur(25px);
-    transform: scaleX(1.1)
-}
-.section-service .content .card {
-    @apply "md:w-1/3 shadow-card pb-8 px-6 rounded-lg bg-white relative";
-}
-.section-service .content .card:nth-child(2) {
-    @apply "my-6 md:mx-6 md:my-0";
-}
-.section-service .content .card > * {
-    @apply "text-center mx-auto";
-}
-.section-service .content .card h3 {
-    @apply "mb-4 text-xl";
-}
-.section-service {
-    @apply "container";
 }
 </style>
