f28b537cc2fcb8569eac9509213d9d29b8fe19bd
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2021-11-18 13:50:57 +0300

    add project section

---
+++ src/components/section/project.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="container">
+        <h1>My project</h1>
+        <div class="py-15">
+            <card-containter :data="articles" size="large" :img="img" />
+        </div>
+    </section>
+</template>
+
+
+<script>
+export default {
+    props: ["articles"],
+    methods: {
+        img(service){
+            return service.image
+        }
+    },
+}
+</script>
