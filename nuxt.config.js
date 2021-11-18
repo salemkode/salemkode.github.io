676fabcd8880cf5cbee51643c549e3f7eab367e1
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2021-11-18 13:26:20 +0300

    support netlify cms

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,7 @@ export default {
     htmlAttrs: {
       lang: 'en',
     },
+    script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
