dc2de4f5fcdde0a447ff6dba4cb8ac868d07a293
salemkode <salemkode@users.noreply.github.com>
2022-01-01 12:12:11 +0000

    deploy: 7055ba2ccf448af7d40ed025abbcc09326b9fdf3

--- _nuxt/static/1640775250/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1640775250",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4b320bae"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1641039131/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fproject\u002Fflipstarter","\u002Fproject","\u002Fproject\u002Flbox"]})
