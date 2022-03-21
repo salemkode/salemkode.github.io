48aa20706268dc45bd7f7b8de99b25b59a8e96f9
salemkode <salemkode@users.noreply.github.com>
2022-03-21 09:27:30 +0000

    deploy: 8ea8964f4937a13f60b05ef434e72b210527db8e

---
+++ _nuxt/static/1647854849/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647854849",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ca583c26"}}}}(null,"\u002F"));
