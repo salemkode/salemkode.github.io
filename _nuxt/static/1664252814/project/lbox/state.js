ccff277f1cb251f83c0b61faaa62353d114e3648
salemkode <salemkode@users.noreply.github.com>
2022-09-27 09:44:24 +0000

    deploy: 3afe4d3effdc0a65d41d50891563f902e2cf9b52

--- _nuxt/static/1664252814/project/lbox/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1664252814",layout:"default",error:a,serverRendered:true,routePath:"\u002Fproject\u002Flbox",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c7253047"}}}}(null));
