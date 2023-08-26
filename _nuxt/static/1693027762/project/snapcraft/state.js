a8a2eecb0d0c50c1593ee53220dcb2fb6a79cb2e
salemkode <salemkode@users.noreply.github.com>
2023-08-26 05:29:23 +0000

    deploy: d03a28132c12716359b9957c96be661c930a893c

---
+++ _nuxt/static/1693027762/project/snapcraft/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1693027762",layout:"default",error:a,serverRendered:true,routePath:"\u002Fproject\u002Fsnapcraft",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9f0ed541"}}}}(null));
